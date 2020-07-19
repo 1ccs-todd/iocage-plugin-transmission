7c66cff32cce1de1fa525373085550039f2b9dc0
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-19 23:10:02 +0200

    More plugin compatibility patches

--- jailman/finish_install.sh
+++ jailman/finish_install.sh
@@ -15,11 +15,18 @@ createmount "$1" "${global_dataset_downloads}"/complete /mnt/downloads/complete
 # Check if dataset InComplete Downloads dataset exist, create if they do not.
 createmount "$1" "${global_dataset_downloads}"/incomplete /mnt/downloads/incomplete
 
-
 iocage exec "$1" chown -R transmission:transmission /config
-iocage exec "$1" sysrc "transmission_enable=YES"
-iocage exec "$1" sysrc "transmission_conf_dir=/config"
-iocage exec "$1" sysrc "transmission_download_dir=/mnt/downloads/complete"
-iocage exec "$1" service transmission restart
 
+# Start service once to create the files needed
+iocage exec "$1" service transmission start
+sleep 2 # It can take a few seconds.
+iocage exec "$1" service transmission stop
+
+# Without the whitelist disabled, the user will not be able to access it. They can reenable and manually whitelist their IP's.
+SETTINGS="/config/settings.json"
+
+echo "Disabling RPC whitelist, you may want to reenable it with the specific IP's you will access transmission with by editing $SETTINGS"
+sed -i '' -e 's/\([[:space:]]*"rpc-whitelist-enabled":[[:space:]]*\)true,/\1false,/' $SETTINGS
+
+iocage exec "$1" service transmission restart
 exitblueprint "$1"
